4074d502d76a139c5328a51e51b1a18cbaad51e2
Leo Allesch <leo.allesch@geappliances.com>
2024-09-04 13:25:49 -0400

    rotates

--- main/src/main.c
+++ main/src/main.c
@@ -7,6 +7,7 @@
 /*********************
  *      INCLUDES
  *********************/
+#include <stdint.h>
 #define _DEFAULT_SOURCE /* needed for usleep() */
 #include <pthread.h>
 #include <stdlib.h>
@@ -63,16 +64,31 @@ int main(int argc, char **argv)
    (void)argc; /*Unused*/
    (void)argv; /*Unused*/
 
+   static uint16_t angle = 0;
+
    /*Initialize LVGL*/
    lv_init();
 
    /*Initialize the HAL (display, input devices, tick) for LVGL*/
    hal_init(240, 240);
 
+   static lv_style_t style;
+   lv_style_init(&style);
+   lv_style_set_bg_color(&style, lv_color_hex(0xcc182d));
+   lv_style_set_radius(&style, 0);
+
+   lv_obj_t *obj = lv_obj_create(lv_screen_active());
+   lv_obj_set_size(obj, 10, 100);
+   lv_obj_set_pos(obj, 120, 120);
+
+   lv_obj_add_style(obj, &style, 0);
+
    while(1)
    {
       /* Periodically call the lv_task handler.
        * It could be done in a timer interrupt or an OS task too.*/
+      lv_obj_set_style_transform_rotation(obj, ((angle += 2)), 0);
+
       lv_timer_handler();
       usleep(5 * 1000);
    }
